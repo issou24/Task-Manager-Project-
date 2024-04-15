c37fc8054b62e7bc0b4994f1861c197035377473
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2024-04-15 05:26:11 +0200

    maj

--- app/completed/page.tsx
+++ app/completed/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import Tasks from "../Components/Tasks/Tasks";
 import { useGlobalState } from "../context/globalProvider";
 
-function page() {
+function Page() {
   const { completedTasks } = useGlobalState();
   return <Tasks title="Completed Tasked" tasks={completedTasks} />;
 }
 
-export default page;
+export default Page;
--- app/incomplete/page.tsx
+++ app/incomplete/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 import Tasks from "../Components/Tasks/Tasks";
 import { useGlobalState } from "../context/globalProvider";
 
-function page() {
+function Page() {
   const { incompleteTasks } = useGlobalState();
   return <Tasks title="Incomplete Tasks" tasks={incompleteTasks} />;
 }
 
-export default page;
+export default Page;
